3a7fff03ea0c18890ab6e73cf58f82b77ab6f0ee
Kendall Holmes <98422018+kendallatada@users.noreply.github.com>
2023-03-31 14:29:43 -0700

    create and test person class (part 1 of activity)

--- tests/test_person.py
+++ tests/test_person.py
@@ -1,9 +1,45 @@
 import pytest
+from viewing_party.person import Person
 
-def test_1():
-    # Arrange
-
-    # Act
+def test_create_person():
+    # Arrange / Act
+    kendall = Person("Kendall", ["Nancy"])
 
     # Assert
-    pass
+    assert isinstance(kendall, Person)
+
+def test_person_name_is_set_correctly():
+    # Arrange / Act
+    nancy = Person("Nancy", ["Kendall"])
+    danica = Person("Danica", ["Ping"])
+
+    # Assert
+    assert nancy.name == "Nancy"
+    assert danica.name == "Danica"
+
+def test_person_friends_list_is_set_correctly():
+    # Arrange / Act
+    ana = Person("Ana", ["Ariel"])
+
+    # Assert
+    assert ana.friends == ["Ariel"]
+
+def test_friend_added_to_person():
+    # Arrange
+    laura = Person("Laura", ["Sage"])
+
+    # Act
+    laura.add_friend("Elizabeth")
+
+    # Assert
+    assert laura.friends == ["Sage", "Elizabeth"]
+
+def test_no_duplicate_friends_added():
+    # Arrange
+    moyo = Person("Moyo", ["Sarah"])
+
+    # Act
+    moyo.add_friend("Sarah")
+
+    # Assert
+    assert moyo.friends == ["Sarah"]
--- viewing_party/person.py
+++ viewing_party/person.py
@@ -1,3 +1,8 @@
 class Person:
-    pass
+    def __init__(self, name, friends):
+        self.name = name
+        self.friends = friends
     
+    def add_friend(self, friend):
+        if friend not in self.friends:
+            self.friends.append(friend)
